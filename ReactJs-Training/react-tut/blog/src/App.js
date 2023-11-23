8cd79c8c80cd2a3ab6e9f03cf016f9f74eeda83b
ashish8135 <ashish.shaivya10@gmail.com>
2023-11-23 08:09:13 +0530

    class component added

--- ReactJs-Training/react-tut/blog/src/App.js
+++ ReactJs-Training/react-tut/blog/src/App.js
@@ -1,15 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import User  from './user';
+// import {User}  from './user';
+import ClassComponent  from './classcompo';
+import React  from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World !</h1>
-      <User></User>
-    </div>
-  );
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World !</h1>
+//       <User></User>
+//       <ClassComponent/>
+//     </div>
+//   );
+// }
+
+
+
+// Class component in react
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      data:"Ashish"
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello {this.state.data}</h1>
+        <User></User>
+        {/* <ClassComponent/> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App;export function User(){
+  return (
+      // <div>
+          // {/* <h1>Function   Component</h1> */}
+          // <h1>user Component</h1>
+      // {/* </div> */}
+      console.log("Hello World")
+  )
+}
+
--- ReactJs-Training/react-tut/blog/src/user.js
+++ ReactJs-Training/react-tut/blog/src/user.js
@@ -1,7 +1,12 @@
-function User(){
+export function User(){
     return (
-    <h1>Function Component</h1>
+        <div>
+            <h1>Function   Component</h1>
+            <h1>user Component</h1>
+        </div>
     )
 }
 
-export default User;
+// export default User;
+// ;
+// We will use export without default in function and in import we will use bracket to import file.
